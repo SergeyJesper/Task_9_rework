e2848140f0ec1fa58be4a61dbae04052bc940c36
SergeyJesper <jesperb@mail.ru>
2019-08-18 11:37:52 +0300

    комментарий 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        System.out.println("Hello, commit");
+        System.out.println("Hello, commit  ");
         //комментарий
 
     }
