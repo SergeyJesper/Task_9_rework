cc1810333ea789112b2be7f22f36c2ec5ea47bf4
SergeyJesper <jesperb@mail.ru>
2019-09-01 09:42:20 +0300

    Задача 11

--- src/String_var.java
+++ src/String_var.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 public class String_var {
     public static void main (String [] arg) {
 
-        String str = "Строка";
-        System.out.println(str);
+        String var;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите какое-нибудь слово: ");
+        var = in.nextLine();
+        System.out.print("Ваше слово: " + var);
     }
 }
